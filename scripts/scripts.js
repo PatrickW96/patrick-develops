982c4662ebb4a9888dd8df4787b45659fbd3ffa3
Patrick W <pat.realrealestatellc@gmail.com>
2020-10-09 13:11:56 -0400

    Update: Added scroll function logic with animations to inhance user fun.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
 
+    $(window).scroll(function() {
+        var topPosition = $(document).scrollTop();
+        console.log(topPosition)
 
-
-
-
-
-
-
-
-
+        if (topPosition > 856) {
+            $('#fact-one').removeClass('hide')
+            $('#fact-one').addClass('animate__animated animate__bounceInLeft')
+        };
+        if (topPosition > 1197) {
+            $('#fact-two').removeClass('hide')
+            $('#fact-two').addClass('animate__animated animate__bounceInRight')
+        };
+        if (topPosition > 1500) {
+            $('#fact-three').removeClass('hide')
+            $('#fact-three').addClass('animate__animated animate__bounceInLeft')
+        };
+        if (topPosition > 1820) {
+            $('#fact-four').removeClass('hide')
+            $('#fact-four').addClass('animate__animated animate__bounceInRight')
+        };
+        if (topPosition > 2155) {
+            $('#fact-five').removeClass('hide')
+            $('#fact-five').addClass('animate__animated animate__bounceInLeft')
+        };
+    });
 });
