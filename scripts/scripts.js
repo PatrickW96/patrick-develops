2f8d492319378c8d24562aae947be2d56e9d132e
Patrick W <pat.realrealestatellc@gmail.com>
2020-10-09 14:18:07 -0400

    Updating files. Fixing box animations for mobile device.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,25 +4,33 @@ $(document).ready(function() {
         var topPosition = $(document).scrollTop();
         console.log(topPosition)
 
-        if (topPosition > 856) {
+        if (topPosition > 685) {
             $('#fact-one').removeClass('hide')
             $('#fact-one').addClass('animate__animated animate__bounceInLeft')
         };
-        if (topPosition > 1197) {
+        if (topPosition > 800) {
             $('#fact-two').removeClass('hide')
             $('#fact-two').addClass('animate__animated animate__bounceInRight')
         };
-        if (topPosition > 1500) {
+        if (topPosition > 925) {
             $('#fact-three').removeClass('hide')
             $('#fact-three').addClass('animate__animated animate__bounceInLeft')
         };
-        if (topPosition > 1820) {
+        if (topPosition > 1050) {
             $('#fact-four').removeClass('hide')
             $('#fact-four').addClass('animate__animated animate__bounceInRight')
         };
-        if (topPosition > 2155) {
+        if (topPosition > 1175) {
             $('#fact-five').removeClass('hide')
             $('#fact-five').addClass('animate__animated animate__bounceInLeft')
         };
+        if (topPosition > 1225) {
+            $('#fact-six').removeClass('hide')
+            $('#fact-six').addClass('animate__animated animate__bounceInRight')
+        };
     });
+
+    if (width > 768) {
+
+    }
 });
